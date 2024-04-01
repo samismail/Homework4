91ed27356581b9140128e388f6ad6190c04b850a
Sam <sismail1@ggc.edu>
2024-04-01 13:43:45 -0400

    Exercise 3

--- src/Exercise3/Triangle.java
+++ src/Exercise3/Triangle.java
@@ -1,9 +1,9 @@
 package Exercise3;
 
 public class Triangle {
-    double side1;
-    double side2;
-    double side3;
+    private double side1;
+    private double side2;
+    private double side3;
 
     public Triangle(double side1, double side2, double side3){
         this.side1 = side1;
@@ -11,10 +11,10 @@ public class Triangle {
         this.side3 = side3;
     }
     public boolean checkSides() throws IllegalTriangleSideException {
-
         if (side1 + side2 < side3 || side2 + side3 < side1 || side1 + side3 < side2) {
             throw new IllegalTriangleSideException("IllegalTriangleSideException");
-        } else {
+        }
+        else {
             return true;
         }
     }
--- src/Exercise3/TriangleTester.java
+++ src/Exercise3/TriangleTester.java
@@ -3,13 +3,14 @@ package Exercise3;
 public class TriangleTester {
     public static void main(String[] args) {
         Triangle triangle1 = new Triangle(2,1,3);
-        Triangle triangle2 = new Triangle(5,8,3);
+        Triangle triangle2 = new Triangle(5.8,8.2,3);
         Triangle triangle3 = new Triangle(20,1,90);
 
+
         try {
-            System.out.println(triangle1.checkSides());
-            System.out.println(triangle2.checkSides());
-            System.out.println(triangle3.checkSides());
+            System.out.println("Triangle 1: " + triangle1.checkSides());
+            System.out.println("Triangle 2: " + triangle2.checkSides());
+            System.out.println("Triangle 3: " + triangle3.checkSides());
 
         }catch (IllegalTriangleSideException e){
             System.out.println("Exception caught " + e.getMessage()); //new method from the
